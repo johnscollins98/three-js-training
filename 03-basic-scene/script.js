38dafaf97a980a52aabb2981cc6e0b9e82aa1eaf
johnscollins98 <johnscollins98@gmail.com>
2022-08-11 08:48:26 +0100

    format lesson 03 files

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,28 +1,28 @@
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 // Create red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 const sizes = {
   width: 800,
-  height: 600,
-};
-const aspectRatio = sizes.width / sizes.height;
+  height: 600
+}
+const aspectRatio = sizes.width / sizes.height
 
 // Set up camera
-const fov = 75; // degrees
-const camera = new THREE.PerspectiveCamera(fov, aspectRatio);
-camera.position.z = 3;
-camera.position.x = 1;
-scene.add(camera);
+const fov = 75 // degrees
+const camera = new THREE.PerspectiveCamera(fov, aspectRatio)
+camera.position.z = 3
+camera.position.x = 1
+scene.add(camera)
 
 // Create renderer
-const canvas = document.querySelector('.webgl');
+const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
-  canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+  canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
