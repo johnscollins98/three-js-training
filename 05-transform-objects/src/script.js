7cc99f8e17a3aa2d08f07ddd322c06b4592d87c7
johnscollins98 <johnscollins98@gmail.com>
2022-08-12 15:14:52 +0100

    lesson 05 transform objects

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -13,7 +13,36 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+// Adjustments can be done any time before `render`
 scene.add(mesh)
+
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1) // equivalent of above
+console.log(mesh.position.length()) // length of vector from center of the scene
+// mesh.position.normalize() // reduces vector length to 1
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+console.log(mesh.scale.length())
+
+// Rotate (with rotation property)
+
+// change order rotations are applied to axis 
+// - make sure to do this before actually ordering
+mesh.rotation.reorder('YXZ') 
+mesh.rotation.y = Math.PI // half a rotation
+mesh.rotation.y = Math.PI / 2 // quarter rotation
+mesh.rotation.x = Math.PI / 4 // eight rotation ...
+
+// axes helper
+scene.add(new THREE.AxesHelper(2))
 
 /**
  * Sizes
@@ -29,6 +58,10 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+console.log(mesh.position.distanceTo(camera.position)) // length of vector from camera position
+
+// Make camera look at cube
+camera.lookAt(mesh.position)
 
 /**
  * Renderer
